fbc0161c6e33cde7b7f5b5cf1f707e6dcf728206
CleeYOpro <cbalaranjith@gmail.com>
2026-04-21 18:37:57 -0700

    Initial commit: Card game project

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+package Cards_Console.src;
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from VS Code! Ready for data structures :)");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package Cards_Console.src;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to BlackJack.");
+
+        Game blackjack = new Game();
+
+
+
+    }
+}
