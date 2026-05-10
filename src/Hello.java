9c220aac84027799373c43dc578666b442c4a38d
CleeYOpro <cbalaranjith@gmail.com>
2026-05-10 13:58:35 -0700

    deleted

--- src/Hello.java
+++
@@ -1,6 +0,0 @@
-package Cards_Console.src;
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello from VS Code! Ready for data structures :)");
-    }
-}
